f3f22fb19932ed3d5997e76391c95f54c942e090
Ondřej Janošík <ondrej.janosik@scrive.com>
2024-09-12 11:21:10 +0200

    Remove unusable modules

--- servant-parameters-unchained/src/Servant/API/Parameters/Query/Sorting/Examples.hs
+++
@@ -1,8 +0,0 @@
--- | As sorting is generally easy to achieve with a fairly simple sum-type query parameter
--- there's no need for a special treatment.
---
--- Rather than trying to provide some fancy solution with pre-defined behavior, I'd rather
--- describe how to achieve sorting in examples.
-
--- TODO: Add examples for sorting.
--- TODO: Add examples file to the cabal file.
